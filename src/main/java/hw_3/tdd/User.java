33bba50840d0b748e6f362899a7ec8e8f5c36fa0
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-10-24 12:31:12 +0300

    Добавили автодокументацию

--- src/main/java/hw_3/tdd/User.java
+++ src/main/java/hw_3/tdd/User.java
@@ -1,23 +1,45 @@
 package hw_3.tdd;
 
+/**
+ * Класс, представляющий пользователя с аутентификацией и административными правами.
+ */
 public class User {
-    String name;
-    String password;
-    boolean isAdmin = false;
-    boolean isAuthenticate = false;
+    String name; // Имя пользователя.
+    String password; // Пароль пользователя.
+    boolean isAdmin = false; // Флаг, указывающий, является ли пользователь администратором.
+    boolean isAuthenticate = false; // Флаг, указывающий, прошел ли пользователь аутентификацию.
 
+    /**
+     * Конструктор класса User.
+     *
+     * @param name     Имя пользователя.
+     * @param password Пароль пользователя.
+     * @param isAdmin  Флаг, указывающий, является ли пользователь администратором.
+     */
     public User(String name, String password, boolean isAdmin) {
         this.name = name;
         this.password = password;
         this.isAdmin = isAdmin;
     }
 
+    /**
+     * Метод для получения информации о том, является ли пользователь администратором.
+     *
+     * @return true, если пользователь администратор, иначе false.
+     */
     public boolean getIsAdmin() {
         return isAdmin;
     }
 
+    /**
+     * Метод для аутентификации пользователя.
+     *
+     * @param name     Имя пользователя для аутентификации.
+     * @param password Пароль пользователя для аутентификации.
+     * @return true, если аутентификация прошла успешно, иначе false.
+     */
     public boolean authenticate(String name, String password) {
         isAuthenticate = this.name.equals(name) && this.password.equals(password);
-        return isAuthenticate;    }
-
+        return isAuthenticate;
+    }
 }
--- src/main/java/hw_3/tdd/UserRepository.java
+++ src/main/java/hw_3/tdd/UserRepository.java
@@ -3,15 +3,29 @@ package hw_3.tdd;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Класс, представляющий хранилище пользователей с функциональностью добавления, поиска по имени и разлогинивания пользователей.
+ */
 public class UserRepository {
 
-    // Тут можно хранить аутентифицированных пользователей
+    // Список, в котором хранятся аутентифицированные пользователи.
     List<User> data = new ArrayList<>();
 
+    /**
+     * Метод для добавления пользователя в хранилище.
+     *
+     * @param user Пользователь, который должен быть добавлен в хранилище.
+     */
     public void addUser(User user) {
         data.add(user);
     }
 
+    /**
+     * Метод для поиска пользователя по имени.
+     *
+     * @param username Имя пользователя, которое нужно найти.
+     * @return true, если пользователь с указанным именем найден, иначе false.
+     */
     public boolean findByName(String username) {
         for (User user : data) {
             if (user.name.equals(username)) {
@@ -21,6 +35,9 @@ public class UserRepository {
         return false;
     }
 
+    /**
+     * Метод для разлогинивания всех пользователей, кроме администраторов.
+     */
     public void logoutAllUsersExceptAdmins() {
         for (User user : data) {
             if (!user.getIsAdmin()) {
@@ -28,5 +45,4 @@ public class UserRepository {
             }
         }
     }
-
 }
