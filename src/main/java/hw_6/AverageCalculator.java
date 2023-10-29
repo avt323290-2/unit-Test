e160d75869e71bcf881e78dc1173dd9c7335e404
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-10-29 10:12:08 +0300

    Добавили код в файлы домашнего задания №6

--- src/main/java/hw_6/AverageCalculator.java
+++ src/main/java/hw_6/AverageCalculator.java
@@ -1,4 +1,45 @@
 package hw_6;
+import java.util.List;
 
+/**
+ * Класс для рассчета среднего значения и сравнения средних значений двух списков чисел.
+ */
 public class AverageCalculator {
+
+    /**
+     * Рассчитывает среднее значение списка чисел.
+     *
+     * @param numbers Список чисел.
+     * @return Среднее значение списка.
+     */
+    public double calculateAverage(List<Integer> numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        double sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum / numbers.size();
+    }
+
+    /**
+     * Сравнивает средние значения двух списков чисел и возвращает соответствующее сообщение.
+     *
+     * @param list1 Первый список чисел.
+     * @param list2 Второй список чисел.
+     * @return Сообщение о сравнении средних значений.
+     */
+    public String compareAverages(List<Integer> list1, List<Integer> list2) {
+        double average1 = calculateAverage(list1);
+        double average2 = calculateAverage(list2);
+
+        if (average1 > average2) {
+            return "Первый список имеет большее среднее значение";
+        } else if (average2 > average1) {
+            return "Второй список имеет большее среднее значение";
+        } else {
+            return "Средние значения равны";
+        }
+    }
 }
--- src/test/java/hw_6/AverageCalculatorTest.java
+++ src/test/java/hw_6/AverageCalculatorTest.java
@@ -1,4 +1,56 @@
 package hw_6;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AverageCalculatorTest {
+    private AverageCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new AverageCalculator();
+    }
+
+    @Test
+    void testCalculateAverageEmptyList() {
+        List<Integer> numbers = new ArrayList<>();
+        double result = calculator.calculateAverage(numbers);
+        assertEquals(0.0, result, 0.001);
+    }
+
+    @Test
+    void testCalculateAverageNonEmptyList() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+        double result = calculator.calculateAverage(numbers);
+        assertEquals(3.0, result, 0.001);
+    }
+
+    @Test
+    void testCompareAveragesFirstGreater() {
+        List<Integer> list1 = List.of(1, 2, 3, 4, 5);
+        List<Integer> list2 = List.of(1, 2, 3);
+        String result = calculator.compareAverages(list1, list2);
+        assertEquals("Первый список имеет большее среднее значение", result);
+    }
+
+    @Test
+    void testCompareAveragesSecondGreater() {
+        List<Integer> list1 = List.of(1, 2, 3);
+        List<Integer> list2 = List.of(1, 2, 3, 4, 5);
+        String result = calculator.compareAverages(list1, list2);
+        assertEquals("Второй список имеет большее среднее значение", result);
+    }
+
+    @Test
+    void testCompareAveragesEqual() {
+        List<Integer> list1 = List.of(1, 2, 3, 4);
+        List<Integer> list2 = List.of(1, 2, 3, 4);
+        String result = calculator.compareAverages(list1, list2);
+        assertEquals("Средние значения равны", result);
+    }
 }
