ab04e823c296ff95f892bf451507a9affefc1b1c
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-10-29 10:13:52 +0300

    Добавили автодокументацию

--- src/test/java/hw_6/AverageCalculatorTest.java
+++ src/test/java/hw_6/AverageCalculatorTest.java
@@ -8,14 +8,23 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * Класс для тестирования класса AverageCalculator.
+ */
 public class AverageCalculatorTest {
     private AverageCalculator calculator;
 
+    /**
+     * Инициализация объекта AverageCalculator перед каждым тестом.
+     */
     @BeforeEach
     void setUp() {
         calculator = new AverageCalculator();
     }
 
+    /**
+     * Тест для метода calculateAverage, когда список чисел пустой.
+     */
     @Test
     void testCalculateAverageEmptyList() {
         List<Integer> numbers = new ArrayList<>();
@@ -23,6 +32,9 @@ public class AverageCalculatorTest {
         assertEquals(0.0, result, 0.001);
     }
 
+    /**
+     * Тест для метода calculateAverage, когда список чисел не пустой.
+     */
     @Test
     void testCalculateAverageNonEmptyList() {
         List<Integer> numbers = List.of(1, 2, 3, 4, 5);
@@ -30,6 +42,9 @@ public class AverageCalculatorTest {
         assertEquals(3.0, result, 0.001);
     }
 
+    /**
+     * Тест для метода compareAverages, когда среднее значение первого списка больше среднего значения второго списка.
+     */
     @Test
     void testCompareAveragesFirstGreater() {
         List<Integer> list1 = List.of(1, 2, 3, 4, 5);
@@ -38,6 +53,9 @@ public class AverageCalculatorTest {
         assertEquals("Первый список имеет большее среднее значение", result);
     }
 
+    /**
+     * Тест для метода compareAverages, когда среднее значение второго списка больше среднего значения первого списка.
+     */
     @Test
     void testCompareAveragesSecondGreater() {
         List<Integer> list1 = List.of(1, 2, 3);
@@ -46,6 +64,9 @@ public class AverageCalculatorTest {
         assertEquals("Второй список имеет большее среднее значение", result);
     }
 
+    /**
+     * Тест для метода compareAverages, когда средние значения списков равны.
+     */
     @Test
     void testCompareAveragesEqual() {
         List<Integer> list1 = List.of(1, 2, 3, 4);
